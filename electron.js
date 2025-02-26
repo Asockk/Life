32590db84a3101abf0d6ce717a9231abfb9e81cb
SYE <sunay.yetim@sas-softec.com>
2025-02-26 12:22:26 +0100

    Add executable project

---
+++ electron.js
@@ -0,0 +1,45 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require('url');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      webSecurity: false,  // Added to help with local resource loading
+      enableRemoteModule: true
+    }
+  });
+
+  // More robust URL resolution
+  const startUrl = process.env.ELECTRON_START_URL || url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  });
+
+  console.log('Loading URL:', startUrl);
+  console.log('Current directory:', __dirname);
+  
+  win.loadURL(startUrl);
+
+  // Open DevTools for debugging
+  win.webContents.openDevTools();
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",  // Entwicklungsquellen
+    "./build/static/**/*.{js,jsx,ts,tsx}",  // Produktionsquellen
+    "./build/index.html"  // Produktions-HTML
+  ],
   theme: {
     extend: {},
   },
