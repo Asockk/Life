a988862a9d98b1a86aa966b1c1a6e23c5d05585c
SYE <sunay.yetim@sas-softec.com>
2025-03-03 20:32:48 +0100

    Update versionnumber

--- src/components/UI/VersionDisplay.js
+++ src/components/UI/VersionDisplay.js
@@ -4,7 +4,7 @@ import React from 'react';
 const VersionDisplay = () => {
   return (
     <div className="text-gray-400 text-xs text-right mt-2 mb-1 mr-2">
-      v1.0.1
+      v1.0.2
     </div>
   );
 };
